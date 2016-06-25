8814f290547e4729bc862581f12ebcdec672cc59
mlding <Mengli.Ding@wnco.com>
2016-06-25 13:11:42 +0800

    pos1 practise, using toFixed(2) and destruction

---
+++ pos_v0/src/main.js
@@ -0,0 +1,18 @@
+//TODO: Please write code in this file.
+function printInventory(inputs) {
+    let outputs = '***<没钱赚商店>购物清单***\n';
+    let total = 0;
+
+    for(let i=0; i<inputs.length; i++) {
+        const {name, count, price, unit} = inputs[i];
+        const sum = count * price;
+        total += sum;
+        const itemDetail = '名称：' + name + '，数量：' + count + unit + '，单价：' + price.toFixed(2) + '(元)，小计：' + sum.toFixed(2) + '(元)\n';
+        outputs +=  itemDetail;
+    }
+
+    const bottomMsg = '----------------------\n总计：' + total.toFixed(2) + '(元)\n**********************';
+    outputs += bottomMsg;
+
+    console.log(outputs);
+}
