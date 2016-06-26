ab8fc64b981df5a47584f66041c2c577b6ae286c
mlding <Mengli.Ding@wnco.com>
2016-06-26 09:55:36 +0800

    [Mengli] - pos0, use array forEach to replace for loop.

--- pos_v0/src/main.js
+++ pos_v0/src/main.js
@@ -3,13 +3,13 @@ function printInventory(inputs) {
     let outputs = '***<没钱赚商店>购物清单***\n';
     let total = 0;
 
-    for(let i=0; i<inputs.length; i++) {
-        const {name, count, price, unit} = inputs[i];
+    inputs.forEach((item) => {
+        const {name, count, price, unit} = item;
         const sum = count * price;
         total += sum;
         const itemDetail = '名称：' + name + '，数量：' + count + unit + '，单价：' + price.toFixed(2) + '(元)，小计：' + sum.toFixed(2) + '(元)\n';
         outputs +=  itemDetail;
-    }
+    });
 
     const bottomMsg = '----------------------\n总计：' + total.toFixed(2) + '(元)\n**********************';
     outputs += bottomMsg;
