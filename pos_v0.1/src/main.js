73e8b8918a8119496653cdf1070540e027e10a16
mlding <Mengli.Ding@wnco.com>
2016-06-27 21:22:15 +0800

    [Mengli] - pos_v0.1 - Array delete repeat obj and display all objects.

--- pos_v0.1/src/main.js
+++ pos_v0.1/src/main.js
@@ -1,4 +1,33 @@
 //TODO: Please write code in this file.
 function printInventory(inputs) {
+    "use strict";
+    let uniqInputs = [];
+    let flagCount = {};
+    inputs.forEach((item) => {
+        if (!flagCount[item.barcode]) {
+            uniqInputs.push(item);
+            flagCount[item.barcode] = 1;
+        } else {
+            flagCount[item.barcode]++;
+        }
+    });
+    uniqInputs.forEach((item) => {
+        item.count = flagCount[item.barcode];
+    });
 
+    let outputs = '***<没钱赚商店>购物清单***\n';
+    let total = 0;
+
+    uniqInputs.forEach((item) => {
+        const {name, count, price, unit} = item;
+        const sum = count * price;
+        total += sum;
+        const itemDetail = '名称：' + name + '，数量：' + count + unit + '，单价：' + price.toFixed(2) + '(元)，小计：' + sum.toFixed(2) + '(元)\n';
+        outputs += itemDetail;
+    });
+
+    const bottomMsg = '----------------------\n总计：' + total.toFixed(2) + '(元)\n**********************';
+    outputs += bottomMsg;
+
+    console.log(outputs);
 }
