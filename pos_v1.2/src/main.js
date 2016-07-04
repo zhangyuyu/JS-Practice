40218288175817b2afcbfcccd07eaa09c176d34e
yuzhang <1580074674@qq.com>
2016-07-04 16:25:39 +0800

    [Yu] - pos_v1.2 -refactor

--- pos_v1.2/src/main.js
+++ pos_v1.2/src/main.js
@@ -1,66 +1,59 @@
-//TODO: Please write code in this file.
 function printInventory(inputs) {
   "use strict";
+  const inputItems = getInputItems(inputs);
   const allItems = loadAllItems();
-  let inputsDetail = [];
-  let flagCount = {};
 
-  inputs.forEach((line) => {
-     const inputArr = line.split("-");
-     const barcode = inputArr[0];
-     const quantity = inputArr[1] ? Number(inputArr[1]): 1;
-
-     if (!flagCount[barcode]) {
-       inputsDetail.push({barcode});
-       flagCount[barcode] = quantity;
-     } else {
-       flagCount[barcode] = flagCount[barcode] + quantity;
-     }
-
-  });
-
-  inputsDetail.forEach((item) => {
-    item.count = flagCount[item.barcode];
-  });
-
-  let outputs = '***<没钱赚商店>购物清单***\n';
-  let promotionItemDetail = '';
   let total = 0;
   let promotionTotal = 0;
+  let itemDetail = '***<没钱赚商店>购物清单***\n';
+  let promotionItemDetail = '----------------------\n挥泪赠送商品：\n'
 
   allItems.forEach((item) => {
-    inputsDetail.forEach((inputItem) => {
-      if(inputItem.barcode === item.barcode) {
-        const {count} = inputItem;
-        const {name, price, unit} = item;
-        let sum = 0
-        let promotionCount = 0;
+        const count = inputItems[item.barcode];
+        if(count > 0){
+            let sum = 0;
+            let promotionCount = 0;
+            const {barcode, name, price, unit} = item;
 
-        if(count > 2 && isPromotion(inputItem.barcode)) {
-            promotionCount = 1;
-            sum = (count - promotionCount) * price;
-            promotionItemDetail += `名称：${name}，数量：${promotionCount}${unit}\n`
-        } else {
-            sum = count * price;
-        }
+            if(count > 2 && isPromotion(barcode)) {
+                promotionCount = parseInt(count / 3, 10);
+                sum = (count - promotionCount) * price;
+                promotionItemDetail += `名称：${name}，数量：${promotionCount}${unit}\n`
+            } else {
+                sum = count * price;
+            }
 
         total += sum;
         promotionTotal += promotionCount * price;
-        const itemDetail = `名称：${name}，数量：${count}${unit}，单价：${price.toFixed(2)}(元)，小计：${sum.toFixed(2)}(元)\n`;
-        outputs += itemDetail;
-      }
-    })
+        itemDetail += `名称：${name}，数量：${count}${unit}，单价：${price.toFixed(2)}(元)，小计：${sum.toFixed(2)}(元)\n`;
+       }
   });
-  const promotionMsg = '----------------------\n挥泪赠送商品：\n'
-  const bottomMsg = '----------------------\n总计：' + total.toFixed(2) + '(元)\n';
-  const bottomPromotionMsg = '节省：' + promotionTotal.toFixed(2) + '(元)\n**********************'
-  outputs = outputs + promotionMsg + promotionItemDetail + bottomMsg + bottomPromotionMsg;
-
-  console.log(outputs);
+  const bottomMsg = `----------------------\n总计：${total.toFixed(2)}(元)\n`;
+  const bottomPromotionMsg = `节省：${promotionTotal.toFixed(2)}(元)\n**********************`
+  console.log(`${itemDetail}${promotionItemDetail}${bottomMsg}${bottomPromotionMsg}`);
 }
 
 function isPromotion(barcode) {
   const promotionItems = loadPromotions();
   const promotionBarCodes = promotionItems[0].barcodes
   return Array(promotionBarCodes).join(",").indexOf(barcode) >-1;
+}
+
+function getInputItems(inputs) {
+    let inputBarcodes = [];
+    let inputItems = {};
+
+    inputs.forEach((line) => {
+        const inputArr = line.split("-");
+        const barcode = inputArr[0];
+        const count = inputArr[1] ? Number(inputArr[1]): 1;
+
+        if(Array(inputBarcodes).join(",").indexOf(barcode) >-1) {
+            inputItems[barcode] += count;
+        } else {
+            inputBarcodes.push(barcode);
+            inputItems[barcode] = count;
+        }
+    });
+    return inputItems;
 }
