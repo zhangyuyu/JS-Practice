e53eb00c2baf762f8286c0df672569df377e7bb2
yuzhang <1580074674@qq.com>
2016-07-06 20:25:12 +0800

    [Yu] - pos_v2 - refactor method to cartItem

--- pos_v2/src/main.js
+++ pos_v2/src/main.js
@@ -1,14 +1,24 @@
 "use strict";
 function printInventory(inputs) {
-    let cartItems = getCartItems(inputs);
+    const title = "***<没钱赚商店>购物清单***\n";
+    const time = `打印时间：${moment().format('YYYY[年]MM[月]DD[日] HH:mm:ss')}\n`;
+    const separateLine = "----------------------\n";
+    const promotionMsg = "挥泪赠送商品：\n"
 
-    cartItems.forEach((item) => {
-        if(item.count > 2 && isPromotion(item.barcode)) {
-            item.promotionCount = parseInt(item.count / 3, 10);
-            item.isPromotion = true;
-        }
+    let cartTotal = 0;
+    let promotionTotal = 0;
+    let cartItemsMsg = "";
+    let promotionItemsMsg = "";
+
+    getCartItems(inputs).forEach((item) => {
+        cartTotal += item.getSubTotal();
+        promotionTotal += item.getPromotionSubTotal();
+        cartItemsMsg += item.getItemDetail();
+        promotionItemsMsg += item.getPromotionDetail();
     });
-    print(cartItems);
+    const totalMsg = `总计：${cartTotal.toFixed(2)}(元)\n节省：${promotionTotal.toFixed(2)}(元)\n**********************`;
+
+    console.log(`${title}${time}${separateLine}${cartItemsMsg}${separateLine}${promotionMsg}${promotionItemsMsg}${separateLine}${totalMsg}`);
 }
 
 function isPromotion(barcode) {
@@ -43,29 +53,4 @@ function getInputMap(inputs) {
         }
     });
     return inputMap;
-}
-
-function print(cartItems) {
-    const title = "***<没钱赚商店>购物清单***\n";
-    const time = `打印时间：${moment().format('YYYY[年]MM[月]DD[日] HH:mm:ss')}\n`;
-    const separateLine = "----------------------\n";
-    const promotionMsg = "挥泪赠送商品：\n"
-
-    let cartTotal = 0;
-    let promotionTotal = 0;
-    let cartItemsMsg = "";
-    let promotionItemsMsg = "";
-
-    cartItems.forEach((item) => {
-       const subTotal = item.price * (item.count - item.promotionCount);
-       cartTotal += subTotal;
-       cartItemsMsg += `名称：${item.name}，数量：${item.count}${item.unit}，单价：${item.price.toFixed(2)}(元)，小计：${subTotal.toFixed(2)}(元)\n`;
-       if(item.isPromotion) {
-           promotionTotal += item.price * item.promotionCount;
-           promotionItemsMsg += `名称：${item.name}，数量：${item.promotionCount}${item.unit}\n`;
-       }
-    });
-    const totalMsg = `总计：${cartTotal.toFixed(2)}(元)\n节省：${promotionTotal.toFixed(2)}(元)\n**********************`;
-
-    console.log(`${title}${time}${separateLine}${cartItemsMsg}${separateLine}${promotionMsg}${promotionItemsMsg}${separateLine}${totalMsg}`);
 }
